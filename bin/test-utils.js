a41f3f7bfe1131ca0c357078e13821d0fbaef09c
Paweł Kornijów <pawel.kornijow@intive.com>
2018-06-25 01:49:02 +0200

    get versioning from package.json

--- bin/test-utils.js
+++ bin/test-utils.js
@@ -2,11 +2,11 @@
 
 const fs = require('fs');
 const path = require('path');
-
 var program = require('commander');
+const pkg = require('../package.json');
  
 program
-  .version('0.1.0')
+  .version(pkg.version)
   .description('Javascript Test Utils') 
   .command('create <name>', 'bootstrap new test suit').alias('c')
   .parse(process.argv);
---
+++ elosik-test.js
@@ -0,0 +1,15 @@
+import Elosik from './elosik.js';
+
+describe('Elosik', () => {
+  beforeEach(() => {
+    
+  });
+
+  it('should be true', () => {
+    expect(true).to.be.true;
+  });
+
+  afterEach(() => {
+
+  });
+});
