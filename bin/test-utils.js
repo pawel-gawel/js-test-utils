2f60c108a938663f8c48abf7472dc4a4a245da85
Paweł Kornijów <pawel.kornijow@intive.com>
2018-06-24 20:16:13 +0200

    load my command

--- bin/test-utils.js
+++ bin/test-utils.js
@@ -1,22 +1,27 @@
 #!/usr/bin/env node
 
+"use strict"
+
 const fs = require('fs');
 const path = require('path');
 const commandName = process.argv[2];
 
 if (!commandName) {
-  console.error('Please provice command name');
+  console.error('Please provide command name');
   process.exit(1);
 }
 
-const commandPath = '../commands/create.js';
-if (fs.existsSync(commandPath)) {
+const commandPath = `../commands/${commandName}.js`;
+console.log(commandPath)
+
+try {
+  const command = require(commandPath);
+  command.run();
+} catch (e) {
   console.error('Command does not exist');
   process.exit(1);
 }
 
-command = require(commandPath);
-command.run();
 
 
-fs.readFileSync(path.resolve(__dirname, '../templates/base.js'));
+// console.log(fs.readFileSync(path.resolve(__dirname, '../templates/base.js')).toString('utf-8'));
