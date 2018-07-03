54dd0fc869a0c6d1a898e1e3136d21c3639d11a6
Paweł Kornijów <pawel.kornijow@intive.com>
2018-07-03 23:17:10 +0200

    tests entrypoint

--- bin/test-watch.sh
+++ bin/test-watch.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./node_modules/.bin/mocha --watch --watch-extensions js,jsx $@
+entrypoint="$(dirname $(realpath $0))/../index.js"
+
+./node_modules/.bin/mocha --watch --watch-extensions js,jsx $entrypoint $@
