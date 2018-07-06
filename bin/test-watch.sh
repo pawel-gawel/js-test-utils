0d26bd16046a1de0faa90c3ef2510742002f806c
Paweł Kornijów <pawel.kornijow@intive.com>
2018-07-07 00:36:09 +0200

    do I need js entrypoint

--- bin/test-watch.sh
+++ bin/test-watch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-entrypoint="$(dirname $(realpath $0))/../index.js"
+#entrypoint="$(dirname $(realpath $0))/../test/index.js"
 
-./node_modules/.bin/mocha --watch --watch-extensions js,jsx $entrypoint $@
+./node_modules/.bin/mocha --watch --watch-extensions js,jsx $@
